97a719f7c938af14f6fa3bc9b2a15d46286bea2a
Pumpkinouu <zivanovicdanilo0@gmail.com>
2023-01-29 01:28:33 +0100

    Added the whole project

---
+++ nb_premier_danzivanovi/nb_premier_danzivanovi/Program.cs
@@ -0,0 +1,70 @@
+﻿//ETML
+//Auteur      : Danilo Zivanovic
+//Date        : 23.12.2022
+//Descritpion : 
+using System;
+
+namespace nb_premier_danzivanovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool recommencer = true;
+
+            Console.Title = ("Nombre Premier");
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("╔════════════════════════════════════════════════════════════════╗");
+                Console.WriteLine("║ Bienvenue dans le programme Nombre Premier by Danilo Zivanovic ║");
+                Console.WriteLine("╚════════════════════════════════════════════════════════════════╝");
+                Console.ResetColor();
+
+
+                // Demander à l'utilisateur d'entrer un nombre
+                Console.Write("Veuillez entrer un nombre : ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                // Vérifier les nombres premiers qui précèdent le nombre saisi par l'utilisateur
+                Console.WriteLine("Les nombres premiers qui précèdent " + number + " sont :");
+                int count = 0;
+                for (int i = 2; i < number; i++)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    // Afficher les nombres premiers en colonne par dizaine
+                    if (isPrime)
+                    {
+                        Console.Write(i + " ");
+                        count++;
+                        if (count % 10 == 0)
+                        {
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                Console.WriteLine();
+                Console.Write("Voulez vous recommencer le programme ? (o / n): ");
+                string oui = "o";
+                if (oui == Console.ReadLine())
+                {
+                    recommencer = true;
+                }
+                else
+                {
+                    recommencer = false;
+                }
+            } while (recommencer == true);
+        }
+    }
+}
